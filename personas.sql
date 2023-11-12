0ad6535d1d4e6817a4f5945cbebc4dc8c8933cc9
unknown <sngrisa@gmail.com>
2023-11-12 16:56:56 -0300

    Ejercicio Terminado

---
+++ personas.sql
@@ -0,0 +1,6 @@
+INSERT INTO `personas` (`id`, `nombre`, `apellido`, `edad`, `fecha`, `provincia`) VALUES (1, 'Sephiroth', 'Crescent', 30, '1985-11-12 16:26:58', 'Córdoba');
+INSERT INTO `personas` (`id`, `nombre`, `apellido`, `edad`, `fecha`, `provincia`) VALUES (2, 'Geralt', 'De Rivia', 40, '1975-5-1 16:27:41', 'Santa Fe');
+INSERT INTO `personas` (`id`, `nombre`, `apellido`, `edad`, `fecha`, `provincia`) VALUES (3, 'Aerith', 'Gainsborough', 22, '1985-2-2 16:28:47', 'Buenos Aires');
+INSERT INTO `personas` (`id`, `nombre`, `apellido`, `edad`, `fecha`, `provincia`) VALUES (4, 'Ifalna', 'Faremis', 30, '1975-9-1 16:29:21', 'Corrientes');
+INSERT INTO `personas` (`id`, `nombre`, `apellido`, `edad`, `fecha`, `provincia`) VALUES (5, 'Makoto', 'Kino', 18, '2023-11-12 16:30:41', 'Misiones');
+INSERT INTO `personas` (`id`, `nombre`, `apellido`, `edad`, `fecha`, `provincia`) VALUES (6, 'Ino', 'Yamanaka', 25, '1994-1-12 16:31:02', 'San Luis');
